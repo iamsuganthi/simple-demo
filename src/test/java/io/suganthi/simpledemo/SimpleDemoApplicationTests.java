622ca9b327ccfb76afb623bcf313cc3045eac0fe
Suganthi Krishnavathi <suganthikrishnavathi@MacBook-Pro.local>
2024-05-20 16:15:21 +1000

    first commit

---
+++ src/test/java/io/suganthi/simpledemo/SimpleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suganthi.simpledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
